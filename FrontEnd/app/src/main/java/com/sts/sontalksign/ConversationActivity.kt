4db618e1055e9b34c71b3a30b5fc65a3c0a35694
HwayeongD <donghwayeong@gmail.com>
2023-10-17 17:29:19 +0900

    [Add] #S09P31A609-124 1.대화시작 페이지로 이동 개발 2. 실시간 대화페이지 레이아웃 개발(진행중)

---
+++ FrontEnd/app/src/main/java/com/sts/sontalksign/ConversationActivity.kt
@@ -0,0 +1,54 @@
+package com.sts.sontalksign
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.sts.sontalksign.databinding.ActivityConversationBinding
+
+class ConversationActivity : AppCompatActivity() {
+    private val CAMERA_PERMISSION = arrayOf(Manifest.permission.CAMERA)
+    private val FLAG_PERM_CAMERA = 98
+    private val FLAG_REQ_CAMERA = 101
+
+    private val binding by lazy {
+        ActivityConversationBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_conversation)
+        setContentView(binding.root)
+
+        openCamera() //카메라 호출
+    }
+
+    private fun openCamera() {
+        //카메라 권한 확인
+        if(checkPermission(CAMERA_PERMISSION, FLAG_PERM_CAMERA)) {
+            val intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            //startActivityForResult()
+        }
+    }
+
+    //권한 체크 - 카메라
+    private fun checkPermission(permissions:Array<out String>, flag:Int):Boolean {
+        //AOS 버전이 Tiramisu(API 33) 이상일 때만 - 현재 z flip3 지원 버전(23.10.17 기준)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            for(permission in permissions) {
+                //권한 미승인 항목은 권한 승인 요청을 사용자 화면에 호출
+                if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, permissions, flag)
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+}
