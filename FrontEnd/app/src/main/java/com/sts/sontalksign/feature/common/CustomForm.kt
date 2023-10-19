38f4ce548f8037c0f4ddab045da4bf01a06d7a28
HwayeongD <donghwayeong@gmail.com>
2023-10-19 14:43:24 +0900

    [Add] #S09P31A609-129 1. 대화저장 input 레이아웃 개발 2. 대화저장 form 레이아웃 개발 3. input 레이아웃의 커스텀 개발

---
+++ FrontEnd/app/src/main/java/com/sts/sontalksign/feature/common/CustomForm.kt
@@ -0,0 +1,46 @@
+package com.sts.sontalksign.feature.common
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.Gravity
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import com.sts.sontalksign.databinding.CustomFormBinding
+
+class CustomForm(private val context : AppCompatActivity) {
+    private lateinit var binding : CustomFormBinding
+    private val fDialog = Dialog(context)
+    private lateinit var listener: CustomFormBtnStoreClickedListener
+
+    fun show(content : String) {
+        binding = CustomFormBinding.inflate(context.layoutInflater)
+
+        fDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        fDialog.setContentView(binding.root)
+        fDialog.setCancelable(false)
+
+        binding.btnStore.setOnClickListener {
+            //저장
+           listener.onBtnStoreClicked("저장되었습니다")
+            fDialog.dismiss()
+        }
+
+        //fDialog.window?.setGravity(Gravity.TOP)
+        fDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        fDialog.show()
+    }
+
+    fun setOnBtnStoreClickedListener(listener: (String) -> Unit) {
+        this.listener =object: CustomFormBtnStoreClickedListener {
+            override fun onBtnStoreClicked(content: String) {
+                listener(content)
+            }
+        }
+    }
+
+    interface CustomFormBtnStoreClickedListener {
+        fun onBtnStoreClicked(content: String)
+    }
+}
