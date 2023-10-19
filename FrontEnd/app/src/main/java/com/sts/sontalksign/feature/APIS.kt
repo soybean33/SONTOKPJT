a49251a23218eb87585b6f7a3c00273ff7cf8853
soybean33 <80668684+soybean33@users.noreply.github.com>
2023-10-19 13:12:10 +0900

    [Add] #S09P31A609-92 1. API 통신을 위한 그래들, 메니페스트 수정 2. API를 모아두는 APIS.kt 생성, 3. DataModel 클래스를 모아두는 DataModels.kt 생성

---
+++ FrontEnd/app/src/main/java/com/sts/sontalksign/feature/APIS.kt
@@ -0,0 +1,34 @@
+package com.sts.sontalksign.feature
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface APIS {
+
+    /*
+    * @POST("api/~~")
+    * fun [함수명](
+    *   @Body   [보낼꺼],
+    * ): Call<[반환]>
+    *
+    *함수명 규칙은 카멜표기법 (signUp)
+    *변수명 규칙은 카멜표기법 (nickName)
+    *
+    * */
+
+    companion object {
+        private const val BASE_URL = "https://j9a406.p.ssafy.io"    // 주소 --> 없으면 빼도 됨
+
+        fun create(): APIS {
+            val gson : Gson = GsonBuilder().setLenient().create();
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+                .create(APIS::class.java)
+        }
+    }
+}
