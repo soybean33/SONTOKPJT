9fda2d69fe1fcb034429aff633a6198e401575a4
soybean33 <80668684+soybean33@users.noreply.github.com>
2023-10-22 13:08:06 +0900

    [Add] 스와이프 동작 내비게이션 바와 동기화 완료

--- FrontEnd/app/src/main/java/com/sts/sontalksign/feature/common/MainActivityNavBar.kt
+++ FrontEnd/app/src/main/java/com/sts/sontalksign/feature/common/MainActivityNavBar.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.sts.sontalksign.R
 import com.sts.sontalksign.databinding.ActivityMainNavBarBinding
@@ -18,13 +19,81 @@ class MainActivityNavBar : AppCompatActivity() {
         ActivityMainNavBarBinding.inflate(layoutInflater)
     }
 
-    private val fragmentState: ArrayList<Boolean> = arrayListOf(false, true, false)
+    private var selectIconColor = 0
+    private var selectBackColor = 0
+    private var unSelectIconColor = 0
+    private var unSelectBackColor = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        selectIconColor = ContextCompat.getColor(this, R.color.rectangle100)
+        selectBackColor = ContextCompat.getColor(this, R.color.rectangle500)
+        unSelectIconColor = ContextCompat.getColor(this, R.color.rectangle500)
+        unSelectBackColor = ContextCompat.getColor(this, R.color.base)
+
+
         initViewPager()
+
+        TabLayoutMediator(binding.tlMainTabLayout, binding.vpMainViewPager) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.icon = ContextCompat.getDrawable(this, R.drawable.baseline_history_24)
+                    val color = ContextCompat.getColor(this, R.color.rectangle500)
+                    tab.icon?.setTint(color)
+                }
+
+                1 -> {
+                    tab.icon = ContextCompat.getDrawable(this, R.drawable.baseline_sign_language_24)
+                    val iconDrawable = tab?.icon
+                    iconDrawable?.setTint(selectIconColor)
+                    tab.view?.setBackgroundColor(selectBackColor)
+                }
+
+                2 -> {
+                    tab.icon = ContextCompat.getDrawable(this, R.drawable.baseline_settings_24)
+                    val color = ContextCompat.getColor(this, R.color.rectangle500)
+                    tab.icon?.setTint(color)
+                }
+            }
+        }.attach()
+
+        binding.tlMainTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                val position = tab?.position
+                when (position) {
+                    0 -> {
+                        val iconDrawable = ContextCompat.getDrawable(this@MainActivityNavBar, R.drawable.baseline_history_24)
+                        iconDrawable?.setTint(selectIconColor)
+                        tab.icon = iconDrawable
+                        tab.view.setBackgroundColor(selectBackColor)
+                    }
+
+                    1 -> {
+                        val iconDrawable = ContextCompat.getDrawable(this@MainActivityNavBar, R.drawable.baseline_sign_language_24)
+                        iconDrawable?.setTint(selectIconColor)
+                        tab.icon = iconDrawable
+                        tab.view.setBackgroundColor(selectBackColor)
+                    }
+
+                    2 -> {
+                        val iconDrawable = ContextCompat.getDrawable(this@MainActivityNavBar, R.drawable.baseline_settings_24)
+                        iconDrawable?.setTint(selectIconColor)
+                        tab.icon = iconDrawable
+                        tab.view.setBackgroundColor(selectBackColor)
+                    }
+                }
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                val iconDrawable = tab?.icon
+                iconDrawable?.setTint(unSelectIconColor)
+                tab?.view?.setBackgroundColor(unSelectBackColor)
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
     }
 
     private fun initViewPager() {
@@ -37,81 +106,7 @@ class MainActivityNavBar : AppCompatActivity() {
             adapter = viewPager2Adatper
 
             setCurrentItem(1, false)
-
-            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-                override fun onPageSelected(position: Int) {
-                    super.onPageSelected(position)
-
-                    when (position) {
-                        0 -> {
-                            setFrag(0)
-                            changeNavColor()
-                        }
-                        1 -> {
-                            setFrag(1)
-                            changeNavColor()
-                        }
-                        2 -> {
-                            setFrag(2)
-                            changeNavColor()
-                        }
-                    }
-                }
-            })
-        }
-
-    }
-
-    private fun changeNavColor() {
-        if(fragmentState[0]) {
-            binding.ivGotoHistory.setColorFilter(getColor(R.color.rectangle100))
-            binding.flGotoHistory.setBackgroundColor(ContextCompat.getColor(this, R.color.rectangle500))
-
-            binding.ivGotoConversation.setColorFilter(getColor(R.color.rectangle500))
-            binding.flGotoConversation.setBackgroundColor(ContextCompat.getColor(this, R.color.base))
-
-            binding.ivGotoSetting.setColorFilter(getColor(R.color.rectangle500))
-            binding.flGotoSetting.setBackgroundColor(ContextCompat.getColor(this, R.color.base))
-        }
-        else if(fragmentState[1]) {
-            binding.ivGotoHistory.setColorFilter(getColor(R.color.rectangle500))
-            binding.flGotoHistory.setBackgroundColor(ContextCompat.getColor(this, R.color.base))
-
-            binding.ivGotoConversation.setColorFilter(getColor(R.color.rectangle100))
-            binding.flGotoConversation.setBackgroundColor(ContextCompat.getColor(this, R.color.rectangle500))
-
-            binding.ivGotoSetting.setColorFilter(getColor(R.color.rectangle500))
-            binding.flGotoSetting.setBackgroundColor(ContextCompat.getColor(this, R.color.base))
-        }
-        else if(fragmentState[2]) {
-            binding.ivGotoHistory.setColorFilter(getColor(R.color.rectangle500))
-            binding.flGotoHistory.setBackgroundColor(ContextCompat.getColor(this, R.color.base))
-
-            binding.ivGotoConversation.setColorFilter(getColor(R.color.rectangle500))
-            binding.flGotoConversation.setBackgroundColor(ContextCompat.getColor(this, R.color.base))
-
-            binding.ivGotoSetting.setColorFilter(getColor(R.color.rectangle100))
-            binding.flGotoSetting.setBackgroundColor(ContextCompat.getColor(this, R.color.rectangle500))
-        }
-    }
-
-    private fun setFrag(fragNum: Int) {
-        when(fragNum){
-            0 -> {
-                fragmentState[0] = true
-                fragmentState[1] = false
-                fragmentState[2] = false
-            }
-            1 -> {
-                fragmentState[0] = false
-                fragmentState[1] = true
-                fragmentState[2] = false
-            }
-            2 -> {
-                fragmentState[0] = false
-                fragmentState[1] = false
-                fragmentState[2] = true
-            }
         }
     }
 }
+
