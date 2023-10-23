8d3b91af2a703b9261b620eaeabcec43f9438a80
임서희 <seooh212@gmail.com>
2023-10-23 17:46:33 +0900

    [Add] #S09P31A609-2151 1. 태그 어댑터 틀 구성 2. Conversation 틀 구성
    Former-commit-id: d4760aa50e06867e3950d20b6b9d37da2d698c82

--- FrontEnd/app/src/main/java/com/sts/sontalksign/feature/history/HistoryDetailConversationAdapter.kt
+++ FrontEnd/app/src/main/java/com/sts/sontalksign/feature/history/HistoryDetailConversationAdapter.kt
@@ -1,4 +1,35 @@
 package com.sts.sontalksign.feature.history
 
-class HistoryDetailConversationAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sts.sontalksign.databinding.ActivityConversationBinding
+
+class HistoryDetailConversationAdapter (val historyConversationList: ArrayList<HistoryDetailConversationModel>) : RecyclerView.Adapter<HistoryDetailConversationAdapter.CustomViewHolder>() {
+
+    inner class CustomViewHolder(private val binding: ActivityConversationBinding) :RecyclerView.ViewHolder(binding.root) {
+        fun bind(historyDetailConversation: HistoryDetailConversationModel) {
+
+        }
+
+    }
+
+    fun bind(historyConversationListModel: HistoryDetailConversationModel) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val binding = ActivityConversationBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CustomViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return historyConversationList.size
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        val historyDetailConversation = historyConversationList[position]
+        holder.bind(historyDetailConversation)
+    }
 }
+
