624a4a8d446f30c0993dcf06dbbae197c1aea015
임서희 <seooh212@gmail.com>
2023-10-25 13:29:15 +0900

    [Add] #S09P31A609-205 1. 디테일 대화 틀 구성 완료
    Former-commit-id: 5e1d270ffa13aa213a47d178e270c80926b51fc9

--- FrontEnd/app/src/main/java/com/sts/sontalksign/feature/history/HistoryDetailConversationAdapter.kt
+++ FrontEnd/app/src/main/java/com/sts/sontalksign/feature/history/HistoryDetailConversationAdapter.kt
@@ -2,31 +2,50 @@ package com.sts.sontalksign.feature.history
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
-import com.sts.sontalksign.databinding.ActivityConversationBinding
-import com.sts.sontalksign.databinding.HistoryDetailConversationBinding
 
-abstract class HistoryDetailConversationAdapter (val historyConversationList: ArrayList<HistoryDetailConversationModel>) : RecyclerView.Adapter<HistoryDetailConversationAdapter.CustomViewHolder>() {
+import com.sts.sontalksign.databinding.HistoryConversationBinding
 
-    inner class CustomViewHolder(private val binding: HistoryDetailConversationBinding) :RecyclerView.ViewHolder(binding.root) {
+
+class HistoryDetailConversationAdapter(private val historyDetailConList: ArrayList<HistoryDetailConversationModel>) :
+    RecyclerView.Adapter<HistoryDetailConversationAdapter.CustomViewHolder>() {
+
+    inner class CustomViewHolder(private val binding: HistoryConversationBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
         fun bind(historyDetailConversation: HistoryDetailConversationModel) {
+            binding.tvHistoryConversationText.text = historyDetailConversation.historyText
 
+            // 왼쪽/오른쪽 여부에 따라 메시지 아이템을 조절
+            val isLeftMessage = historyDetailConversation.isLeft
+            val layoutParams = binding.llhHistoryConversationText.layoutParams as ViewGroup.MarginLayoutParams
+            if (isLeftMessage) {
+                // 왼쪽 메시지
+                layoutParams.layoutDirection = ConstraintLayout.LayoutParams.PARENT_ID
+                layoutParams.layoutDirection = ConstraintLayout.LayoutParams.UNSET
+            } else {
+                // 오른쪽 메시지
+                layoutParams.layoutDirection = ConstraintLayout.LayoutParams.UNSET
+                layoutParams.layoutDirection = ConstraintLayout.LayoutParams.PARENT_ID
+            }
+            binding.llhHistoryConversationText.layoutParams = layoutParams
         }
-
-    }
-
-    fun bind(historyConversationListModel: HistoryDetailConversationModel) {
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-        val binding = HistoryDetailConversationBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = HistoryConversationBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
         return CustomViewHolder(binding)
     }
+
     override fun getItemCount(): Int {
-        return historyConversationList.size
+        return historyDetailConList.size
     }
 
-
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        holder.bind(historyDetailConList[position])
+    }
 }
 
