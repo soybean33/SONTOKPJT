1b2f0a10362f1865927e70fcc77536c1a50c3ca8
HwayeongD <donghwayeong@gmail.com>
2023-10-27 15:47:52 +0900

    [Fix] 1. merge error
    Former-commit-id: a0201f5d1a4ae749b0ee18797332fd7e49384579

--- FE/FrontEnd/app/src/main/java/com/sts/sontalksign/feature/common/CommonTagAdapter.kt
+++ FE/FrontEnd/app/src/main/java/com/sts/sontalksign/feature/common/CommonTagAdapter.kt
@@ -1,35 +1,47 @@
 package com.sts.sontalksign.feature.common
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
-import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sts.sontalksign.R
 
-class CommonTagAdapter(private val tags: MutableList<CommonTagItem>) : BaseAdapter() {
-    override fun getCount(): Int = tags.size
+/**
+ * 대화내용 저장 FORM에서 사용하는 TAG 리스트의 Adapter
+ * tags에는 현재 저장된 개인의 태그 정보가 저장되어 있습니다.
+ */
+class CommonTagAdapter(private val tags: List<CommonTagItem>) : RecyclerView.Adapter<CommonTagAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tagView: CardView = itemView.findViewById(R.id.mcv_tag)
+        val tagContent: TextView = itemView.findViewById(R.id.tv_tag)
+    }
 
-    override fun getItem(position: Int): CommonTagItem = tags[position]
+    override fun getItemCount(): Int = tags.size
 
-    override fun getItemId(position: Int): Long = position.toLong()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // MaterialCardView으로 지정한 TAG
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.history_tag_item, parent, false)
+        return ViewHolder(view)
+    }
 
-    override fun getView(position: Int, view: View?, parent: ViewGroup?): View {
-        var convertView = view
-        if(convertView == null) convertView = LayoutInflater.from(parent?.context).inflate(R.layout.history_tag_item, parent, false)
+    override fun onBindViewHolder(holder: CommonTagAdapter.ViewHolder, position: Int) {
+        Log.d("CommonTagAdapter", position.toString())
+        val item = tags[position]
+        //TODO: 색상의 개수 상수화
+        holder.tagView.setCardBackgroundColor((TagSingleton.colorList[position % 10])) // MaterialCardView의 배경색상 지정
+        holder.tagContent.text = item.tagText // TextView의 텍스트 지정
 
-        val item: CommonTagItem = tags[position]
-        val tagView = convertView!!.findViewById<CardView>(R.id.mcv_tag)
-        val tagContent = convertView.findViewById<TextView>(R.id.tv_tag)
-        tagView.setCardBackgroundColor((TagSingleton.colorList[position % 10]))
-
-        tagContent.text = item.tagText
-
-        return convertView
+        val layoutParams = holder.tagView.layoutParams as GridLayoutManager.LayoutParams
+        layoutParams.width = if (position % 2 == 0) {
+            2
+        } else {
+            1
+        }
+        holder.tagView.layoutParams = layoutParams
     }
 }
