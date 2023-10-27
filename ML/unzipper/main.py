40d9112becdcc3c8cebceecf8fe7d67bf7d45d4b
강성구 <sungku2757@gmail.com>
2023-10-27 14:11:14 +0900

    [Fix] #S09P31A609-202 1. 압축 해제 코드 수정
    Former-commit-id: d61908785f4199068c81ea7a7fc61bccd11b3bd4

--- ML/unzipper/main.py
+++ ML/unzipper/main.py
@@ -4,10 +4,17 @@ import sys
 
 input = sys.stdin.readline
 
-path_dir = input("경로 입력: (예시- ./data/) ").strip()
-file_list = os.listdir(path_dir)
 
-for zip_file in file_list:
-    if not zip_file.endswith('.zip'):
-        continue
-    zipfile.ZipFile(path_dir + zip_file, 'r').extractall(path_dir + zip_file[:-4])
+def unzipping(path_dir, save_dir):
+    file_list = os.listdir(path_dir)
+
+    for zip_file in file_list:
+        if not zip_file.endswith('.zip'):
+            continue
+        zipfile.ZipFile(path_dir + zip_file, 'r').extractall(save_dir + zip_file[:-4])
+
+if __name__ == "__main__":
+    path_dir = input("압축 데이터 경로 입력 (예시- ./ai-data/) : ").strip()
+    save_dir = input("저장 경로 입력 (예시- ./ai-data/) : ").strip()
+
+    unzipping(path_dir, save_dir)
