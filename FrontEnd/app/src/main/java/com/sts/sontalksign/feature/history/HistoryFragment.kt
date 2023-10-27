be5a9d181798e6cc5dc0f4684b566afd672ff12a
임서희 <seooh212@gmail.com>
2023-10-27 14:13:55 +0900

    [App] #S09P31A609-212 1. 프레그먼트 히스토리 데이터 바인딩 성공 2. Tag도 비슷하게

--- FrontEnd/app/src/main/java/com/sts/sontalksign/feature/history/HistoryFragment.kt
+++ FrontEnd/app/src/main/java/com/sts/sontalksign/feature/history/HistoryFragment.kt
@@ -1,7 +1,8 @@
 package com.sts.sontalksign.feature.history
 
 import android.os.Bundle
-import android.preference.PreferenceManager
+import android.text.TextUtils.split
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,102 +10,150 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sts.sontalksign.R
 import com.sts.sontalksign.databinding.FragmentHistoryBinding
-
+import com.sts.sontalksign.feature.common.CommonTagItem
+import com.sts.sontalksign.feature.common.TagSingleton
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class HistoryFragment : Fragment() {
 
     private lateinit var binding: FragmentHistoryBinding
     private lateinit var historyListAdapter: HistoryListAdapter
 
-//    private val api = APIS.create();
-
     var historyList: ArrayList<HistoryListModel> = ArrayList()
 
+    private val TAG: String = "HistoryFragment"
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        super.onCreate(savedInstanceState)
+    //내부저장소 - txt 파일
+    private val dataFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    private val timeFormat = SimpleDateFormat("HH:mm")
 
+    private var directory: String? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         // 데이터 바인딩 초기화
-        val view = inflater.inflate(R.layout.fragment_history, container, false)
-        binding = FragmentHistoryBinding.bind(view)
+        binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        // 리사이클러뷰 초기화
-        val recyclerViewList = binding.rvHistoryItem
-        recyclerViewList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-        recyclerViewList.setHasFixedSize(false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        directory = requireActivity().filesDir.absolutePath
 
-// 더미 데이터 생성
-        val dummyData1 = HistoryListModel(
-            historyTitle = "Title 1",
-            EndedTime = 1,
-            historyitemmodel = arrayListOf(
-                HistoryItemModel(historyItemTag = "1번 태그"),
-                HistoryItemModel(historyItemTag = "2번 태그입니다"),
-                HistoryItemModel(historyItemTag = "강한성구"),
-                HistoryItemModel(historyItemTag = "용우오빠코테홧팅")
-            )
-        )
-
-        val dummyData2 = HistoryListModel(
-            historyTitle = "Title 2",
-            EndedTime = 2,
-            historyitemmodel = arrayListOf(
-                HistoryItemModel(historyItemTag = "동동이언니"),
-                HistoryItemModel(historyItemTag = "할아버지"),
-                HistoryItemModel(historyItemTag = "구텐탁 팀장님")
-            )
-        )
-
-        // 더미 데이터를 리스트에 추가
-        historyList.add(dummyData1)
-        historyList.add(dummyData2)
-
-
-        // 어댑터 설정 (여기서 dataList는 데이터 리스트로 대체해야 합니다)
         historyListAdapter = HistoryListAdapter(historyList)
-        binding.rvHistoryItem.adapter = historyListAdapter
+        binding.rvHistoryItem.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = historyListAdapter
+        }
 
+        loadTagList()
 
-//        historyList.clear()
-
-
-//        val pref = PreferenceManager.getDefaultSharedPreferences(requireContext())  // import androidx.preference.PreferenceManager 인지 확인
-//        val token = pref.getString("SERVER_ACCESS_TOKEN", "1")
-
-//
-
-        return view
-
-
-    }
-
-    private fun showNoDataMessage() {
-        // 데이터가 없을 때 특정 메시지를 표시하는 로직을 여기에 구현합니다.
-        // 예를 들어, TextView에 메시지를 설정하거나 다이얼로그를 표시할 수 있습니다.
-        // 예제로 TextView에 메시지 설정하는 방법을 보여드리겠습니다.
-
-        val noDataMessage = "대화한 내역이 없습니다." // 표시할 메시지를 지정
-//        binding.tvViewNoData.text = noDataMessage
-//        binding.tvViewNoData.visibility = View.VISIBLE // TextView를 화면에 표시
+        // Safe call for directory
+        val dirPath = directory
+        if (dirPath != null) {
+            // 히스토리 데이터 로드
+            readTextFile(dirPath)
+        }
     }
 
 
-    private fun saveData() {
-        val pref = PreferenceManager.getDefaultSharedPreferences(requireContext())
-        val edit = pref.edit()
 
-        // 토큰 정보를 저장
-        val token = "your_token_here" // 실제 토큰 정보로 대체
-        edit.putString("[키]", "[데이터]")
+    fun loadTagList() {
+        // tagList는 최초 1회만 로드
+        if (TagSingleton.tagList.size > 0) return
+
+        Log.d(TAG, "Directory : $directory")
+        val file = File(directory.toString())
+
+        // 파일 미존재
+        if (!file.exists()) {
+            Log.d(TAG, "TAGS file does not exist!!")
+            file.mkdirs()
+        }
+
+        directory = requireActivity().filesDir.absolutePath //내부경로의 절대 경로
+
+        val tagFN = "TAGS.txt"
+        val fPath = "$directory/$tagFN"
+        val reader = FileReader(fPath)
+        val buffer = BufferedReader(reader)
+
+        var line: String?
+
+        while (true) {
+            line = buffer.readLine() // 줄 단위로 read
+            if (line == null) break
+            else {
+                val (idx, name) = line.split(" ")
+                TagSingleton.tagList.add(CommonTagItem(idx, name))
+            }
+        }
+
+        val colorList = resources.getIntArray(R.array.tagColorArr)
+        for (color in colorList) {
+            TagSingleton.colorList.add(color)
+        }
+        TagSingleton.tagList.add(CommonTagItem("0", "TEST"))
+
+        buffer.close()
+    }
 
 
-        // 닉네임 정보도 동일하게 저장할 수 있습니다.
-        val nickname = "your_nickname_here" // 실제 닉네임 정보로 대체
-        edit.putString("[키]", "[데이터]")
+    private fun readFileContents(filePath: String): String {
+        val file = File(filePath)
+        if (!file.exists()) return ""
+
+        return file.bufferedReader().use { it.readText() }
+    }
+
+    private fun readTextFile(directoryPath: String) {
+
+        directory = requireActivity().filesDir.absolutePath //내부경로의 절대 경로
 
 
-        edit.apply()    // 적용
+        if (directory != null) {
+            val formatter = SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault())
+            val files = File(directory).listFiles() // Using local val here
+
+            files?.forEach { file ->
+                if (file.isFile && file.name.endsWith(".txt")) {
+                    if (file.name != "TAGS.txt"){
+                        val fileContents = readFileContents(file.absolutePath)
+                        Log.d("무어머야러ㅐㅑ너래냐", fileContents)
+                        val lines = fileContents.lines()
+
+                        if (lines.size >= 3) {
+                            val title = lines[0]
+                            val tagsLine = lines[1]
+                            val tagsList = tagsLine.substringAfter("TAGS_").split("_")
+
+                            val tagItems = tagsList.mapNotNull { tagId ->
+                                TagSingleton.tagList.find { it.tagInd == tagId }
+                            }
+
+//                        val fileName = file.absoluteFile
+                            val date = formatter.parse(file.name.split(".")[0])
+                            val endedTime = date?.time ?: 0L
+
+                            historyList.add(HistoryListModel(title, endedTime, ArrayList(tagItems)))
+                        }
+                    }
+
+
+                }
+            }
+        }
 
     }
 }
+
+
+
+
