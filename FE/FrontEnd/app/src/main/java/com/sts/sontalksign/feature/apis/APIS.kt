64e184a8133bbb4802b273beca42ba5eb33dc456
HwayeongD <donghwayeong@gmail.com>
2023-10-30 15:17:45 +0900

    [Fix] Merge FIx
    Former-commit-id: 26bd428a38a0d0140aa8919f2553b2928a90ea0a

--- FE/FrontEnd/app/src/main/java/com/sts/sontalksign/feature/apis/APIS.kt
+++ FE/FrontEnd/app/src/main/java/com/sts/sontalksign/feature/apis/APIS.kt
@@ -2,8 +2,13 @@ package com.sts.sontalksign.feature.apis
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface APIS {
 
@@ -18,8 +23,9 @@ interface APIS {
     *
     * */
 
+
     companion object {
-        private const val BASE_URL = "https://j9a406.p.ssafy.io"    // 주소 --> 없으면 빼도 됨
+        private const val BASE_URL = "https://naveropenapi.apigw.ntruss.com/"    // 주소 --> 없으면 빼도 됨
 
         fun create(): APIS {
             val gson : Gson = GsonBuilder().setLenient().create();
