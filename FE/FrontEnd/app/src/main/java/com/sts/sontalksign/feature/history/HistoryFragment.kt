7ef32c0172f486949eebdecc0d3b8e4c0338b0a4
임서희 <seooh212@gmail.com>
2023-10-30 16:05:17 +0900

    [Add] #S09P31A609-302 1. 히스토리 프래그먼트 태그 바인딩
    Former-commit-id: 2855b97e8081305ac9a437f48ac982da3b7b81f6

--- FE/FrontEnd/app/src/main/java/com/sts/sontalksign/feature/history/HistoryFragment.kt
+++ FE/FrontEnd/app/src/main/java/com/sts/sontalksign/feature/history/HistoryFragment.kt
@@ -1,22 +1,17 @@
 package com.sts.sontalksign.feature.history
 
 import android.os.Bundle
-import android.text.TextUtils.split
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.sts.sontalksign.R
+import com.google.common.flogger.context.Tags
 import com.sts.sontalksign.databinding.FragmentHistoryBinding
 import com.sts.sontalksign.feature.common.CommonTagItem
 import com.sts.sontalksign.feature.common.TagSingleton
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.io.BufferedReader
 import java.io.File
-import java.io.FileReader
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -27,11 +22,6 @@ class HistoryFragment : Fragment() {
 
     var historyList: ArrayList<HistoryListModel> = ArrayList()
 
-    private val TAG: String = "HistoryFragment"
-
-    //내부저장소 - txt 파일
-    private val dataFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-    private val timeFormat = SimpleDateFormat("HH:mm")
 
     private var directory: String? = null
 
@@ -53,57 +43,15 @@ class HistoryFragment : Fragment() {
             adapter = historyListAdapter
         }
 
-        loadTagList()
 
         // Safe call for directory
         val dirPath = directory
         if (dirPath != null) {
             // 히스토리 데이터 로드
-            readTextFile(dirPath)
+            readTextFile()
         }
     }
 
-
-
-    fun loadTagList() {
-        // tagList는 최초 1회만 로드
-        if (TagSingleton.tagList.size > 0) return
-
-        Log.d(TAG, "Directory : $directory")
-        val file = File(directory.toString())
-
-        // 파일 미존재
-        if (!file.exists()) {
-            Log.d(TAG, "TAGS file does not exist!!")
-            file.mkdirs()
-        }
-
-        directory = requireActivity().filesDir.absolutePath //내부경로의 절대 경로
-
-        val tagFN = "TAGS.txt"
-        val fPath = "$directory/$tagFN"
-        val reader = FileReader(fPath)
-        val buffer = BufferedReader(reader)
-
-        var line: String?
-
-        while (true) {
-            line = buffer.readLine() // 줄 단위로 read
-            if (line == null) break
-            else {
-                val (idx, name) = line.split(" ")
-                TagSingleton.tagList.add(CommonTagItem(idx, name))
-            }
-        }
-
-        val colorList = resources.getIntArray(R.array.tagColorArr)
-        for (color in colorList) {
-            TagSingleton.colorList.add(color)
-        }
-        TagSingleton.tagList.add(CommonTagItem("0", "TEST"))
-
-        buffer.close()
-    }
 
 
     private fun readFileContents(filePath: String): String {
@@ -113,11 +61,11 @@ class HistoryFragment : Fragment() {
         return file.bufferedReader().use { it.readText() }
     }
 
-    private fun readTextFile(directoryPath: String) {
+    private fun readTextFile() {
 
         directory = requireActivity().filesDir.absolutePath //내부경로의 절대 경로
 
-
+        Log.d("디렉토리확인", directory.toString())
         if (directory != null) {
             val formatter = SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault())
             val files = File(directory).listFiles() // Using local val here
@@ -131,24 +79,35 @@ class HistoryFragment : Fragment() {
 
                         if (lines.size >= 3) {
                             val title = lines[0]
-                            val tagsLine = lines[1]
-                            val tagsList = tagsLine.substringAfter("TAGS_").split("_")
 
-                            val tagItems = tagsList.mapNotNull { tagId ->
-                                TagSingleton.tagList.find { it.tagInd == tagId }
+                            val tagsLine = lines[1]
+
+                            val tagsList = tagsLine.substringAfter("TAGS_").trim().split("_")
+
+                            val tagItems = ArrayList<CommonTagItem>()
+                            tagsList.forEach { tagId ->
+                                val tagIdInt = tagId.toIntOrNull() // tagId를 안전하게 정수로 변환
+                                if (tagIdInt != null && tagIdInt >= 0 && tagIdInt < TagSingleton.tagList.size) {
+                                    // 유효한 인덱스 범위 내에 있는 경우에만 처리
+                                    tagItems.add(CommonTagItem(tagId, TagSingleton.tagList[tagIdInt].tagText))
+                                } else {
+                                    // 유효하지 않은 tagId에 대한 처리 (예: 로깅)
+                                    Log.e("태그ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ", "태그아이디 아닙니다: $tagId")
+                                }
                             }
 
-//                        val fileName = file.absoluteFile
                             val date = formatter.parse(file.name.split(".")[0])
-                            val endedTime = date?.time ?: 0L
 
-                            historyList.add(HistoryListModel(title, endedTime, ArrayList(tagItems)))
+                            // 원하는 출력 형식으로 SimpleDateFormat을 다시 사용하여 포맷
+                            val outputFormat = SimpleDateFormat("yyyy년 MM월 dd일", Locale.getDefault())
+                            val formattedDate = outputFormat.format(date)
+
+                            historyList.add(HistoryListModel(title, formattedDate, tagItems))
                         }
                     }
-
-
                 }
             }
+
         }
 
     }
