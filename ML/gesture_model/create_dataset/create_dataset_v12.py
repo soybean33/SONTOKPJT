8eedf5ff770d75fad327e2b1cf3565fba2ea8e1f
강성구 <sungku2757@gmail.com>
2023-11-28 08:42:10 +0900

    [Fix] 1. create_dataset_v12
    Former-commit-id: 28c62e739d96149156421eadeb3a8b06fbd88b1a [formerly 64f1630de44f8c6aca606a6a51e466277307b8a3]
    Former-commit-id: 31f22d684aad7c282a26cb5df443bcb149d95f89

--- ML/gesture_model/create_dataset/create_dataset_v12.py
+++ ML/gesture_model/create_dataset/create_dataset_v12.py
@@ -115,7 +115,6 @@ def create_data(file_root:str, file_list:list, hand_model_path: str, pose_model_
 
         print(f"action: {actions[video_label]}, file name : {video}")
         print(f'real save label idx is :{label_idx}')
-        f = open(f"test/text/test.txt", 'w')
         
         while cap.isOpened():
             frame_data = []
@@ -126,9 +125,6 @@ def create_data(file_root:str, file_list:list, hand_model_path: str, pose_model_
             img = mp.Image(image_format=mp.ImageFormat.SRGB, data=img)
             hand_result = mp_hands.detect_for_video(img, int(cap.get(cv2.CAP_PROP_POS_MSEC)))
             pose_result = mp_pose.detect_for_video(img, int(cap.get(cv2.CAP_PROP_POS_MSEC)))
-            for i in range(len(hand_result.hand_landmarks)):
-                f.write("hand"+str(hand_result.hand_landmarks[0][i])+"\n")
-            f.write("pose"+str(pose_result.pose_landmarks[0][0])+"\n")
 
 
             if pose_result.pose_landmarks and (hand_result.hand_landmarks or video_label == ''):
@@ -218,7 +214,6 @@ def create_data(file_root:str, file_list:list, hand_model_path: str, pose_model_
     # 라벨 데이터 저장
     if dataset_type == "train":
         np.save(os.path.join(dataset_save_path, f'labels'), sorted(list(actions.keys()), key=lambda x:actions[x]))
-    f.close()
     return
 
 
